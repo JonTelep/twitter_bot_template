efdbabf4e2b75819bb797a81655932f114df20d9
jontelep <telepjonathan@gmail.com>
2023-01-29 23:21:02 -0500

    Refactored added Twitter directory to clean up the repo.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 # Below adds the Twitter_Bots/Libraries/ directory as a sys path giving the ability to reference the libraries.
-lib_tweepy = os.path.dirname(os.path.realpath("lib_tweepy.py"))
+lib_tweepy = os.path.dirname(os.path.realpath("lib_tweepy.py")) + '/Twitter'
 sys.path.append(lib_tweepy)
 
 # importing lib_tweepy class for usage
@@ -29,6 +29,6 @@ tweepy_create_object = lib_tweepy(tweet_content, "DONOTREPLY","123")
 # Executing Create Tweet function storing the tweet id from the response
 tweet_id = lib_tweepy.Create_Tweet(tweepy_create_object)
 
-reply_tweet_content = "This is your first reply to your own tweet!"
+reply_tweet_content = "If you're seeing this, you're going to make it."
 tweepy_reply_object = lib_tweepy(reply_tweet_content, tweet_id, "123")
 reply_tweet_id = lib_tweepy.Reply_Tweet(tweepy_reply_object)
