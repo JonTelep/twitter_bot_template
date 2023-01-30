d33c7c0f5bbd4dfe1adcd234748666de982fad06
jontelep <telepjonathan@gmail.com>
2023-01-30 00:04:37 -0500

    Cleaned up tweepy objects, added divide_text function to accurately tweet with replies the full chatGPT response if the reply goes above 280 character limit of a tweet.

--- main.py
+++ main.py
@@ -10,7 +10,6 @@
 # 4. Enter your newly generated tokens into the .env file
 # 5. Send your first openai request!
 # ================================================================
-
 import os
 import sys
 
@@ -23,16 +22,39 @@ lib_openai = os.path.dirname(os.path.realpath("lib_openai.py")) + '/Openai'
 sys.path.append(lib_openai)
 
 from lib_tweepy import lib_tweepy
-from Openai.lib_openai import chatGPT_Request
+from lib_openai import chatGPT_Request
+
+# This helper function will divide text into sections of 280 characters to abide by Twitter's 280 character tweet limit
+def divide_text(text):
+    chunks = []
+    while len(text) > 280:
+        chunk = text[:280].rsplit(' ', 1)[0]
+        chunks.append(chunk)
+        text = text[len(chunk):].lstrip()
+    if text:
+        chunks.append(text)
+    return chunks
 
 # Storing chatGPT's response of the defaulted prompt
-chatgpt_response = chatGPT_Request("Tell me a joke about the Ethereum blockchain.")
-print(chatgpt_response)
+chatgpt_response = chatGPT_Request("Tell me the current price of Doge Coin.")
+divided_response = divide_text(chatgpt_response)
 
-# Creating lib_tweepy object
-tweepy_create_object = lib_tweepy(chatgpt_response, "DONOTREPLY","123")
-
-# Executing Create Tweet function storing the tweet id from the response
-tweet_id = lib_tweepy.Create_Tweet(tweepy_create_object)
-
-print("Your first Tweet ID is: " + tweet_id)
+# Index tracker to keep track of tweet ids and accurately tweet a reply if chatGPT response is longer than 280 characters
+i = 0
+for r in divided_response:
+    if i > 0 :
+        if i > 1 :
+            tweepy_reply_object = lib_tweepy(r, reply_tweet_id)
+        else:
+            tweepy_reply_object = lib_tweepy(r, tweet_id)
+        reply_tweet_id = lib_tweepy.Reply_Tweet(tweepy_reply_object)
+        print("Reply Tweeted" + reply_tweet_id)
+        print(r)
+    else:
+        # Creating lib_tweepy object
+        tweepy_create_object = lib_tweepy(r)
+        # Executing Create Tweet function storing the tweet id from the response
+        tweet_id = lib_tweepy.Create_Tweet(tweepy_create_object)
+        print("Tweeted" + tweet_id)
+        print(r)
+    i += 1
